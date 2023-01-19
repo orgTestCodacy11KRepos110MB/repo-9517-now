134ddb6a294d81e3d325fc8298734e11062cef21
Joschka Braun <joschka.braun@gmail.com>
2023-01-19 11:52:44 +0100

    fix: fix jina version in gateway

--- now/constants.py
+++ now/constants.py
@@ -4,10 +4,10 @@ from docarray.typing import Image, Text, Video
 
 from now.utils import BetterEnum
 
-NOW_GATEWAY_VERSION = '0.0.1-refactor-custom-gateway-32'
-NOW_PREPROCESSOR_VERSION = '0.0.120-refactor-custom-gateway-32'
-NOW_ELASTIC_INDEXER_VERSION = '0.0.142-refactor-custom-gateway-32'
-NOW_AUTOCOMPLETE_VERSION = '0.0.8-refactor-custom-gateway-32'
+NOW_GATEWAY_VERSION = '0.0.1-refactor-custom-gateway-33'
+NOW_PREPROCESSOR_VERSION = '0.0.120-refactor-custom-gateway-33'
+NOW_ELASTIC_INDEXER_VERSION = '0.0.142-refactor-custom-gateway-33'
+NOW_AUTOCOMPLETE_VERSION = '0.0.8-refactor-custom-gateway-33'
 
 
 class Apps(BetterEnum):
